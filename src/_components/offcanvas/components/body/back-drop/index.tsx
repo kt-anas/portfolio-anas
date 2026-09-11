7861c06b194b8db5268c70651e4c15fc435b5b32
anas <ktmanas2020@gmail.com>
2026-09-11 12:42:27 +0530

    menu change

--- src/_components/offcanvas/components/body/back-drop/index.tsx
+++ src/_components/offcanvas/components/body/back-drop/index.tsx
@@ -5,17 +5,30 @@ import { motion, type Variants } from 'framer-motion';
 const easing = [0.76, 0, 0.24, 1] as const;
 
 export function OffcanvasBackdrop() {
-    const windowHeight = typeof window !== 'undefined' ? window.innerHeight : 0;
+    const windowHeight =
+        typeof window !== 'undefined' ? window.innerHeight : 0;
 
-    const initialPath = `M100 0 L200 0 L200 ${windowHeight} L100 ${windowHeight} Q-100 ${windowHeight / 2
-        } 100 0`;
-    const targetPath = `M100 0 L200 0 L200 ${windowHeight} L100 ${windowHeight} Q100 ${windowHeight / 2
-        } 100 0`;
+    const initialPath = `
+        M100 0
+        L200 0
+        L200 ${windowHeight}
+        L100 ${windowHeight}
+        Q-300 ${windowHeight / 2} 100 0
+    `;
+
+    const targetPath = `
+        M100 0
+        L200 0
+        L200 ${windowHeight}
+        L100 ${windowHeight}
+        Q100 ${windowHeight / 2} 100 0
+    `;
 
     const curve: Variants = {
         initial: {
             d: initialPath,
         },
+
         enter: {
             d: targetPath,
             transition: {
@@ -24,6 +37,7 @@ export function OffcanvasBackdrop() {
                 delay: 0.1,
             },
         },
+
         exit: {
             d: initialPath,
             transition: {
@@ -35,12 +49,16 @@ export function OffcanvasBackdrop() {
     };
 
     return (
-        <motion.svg className='absolute right-[600px] top-0 h-full w-24 fill-foreground stroke-none'>
+        <motion.svg
+            className="absolute right-[550px] top-0 h-full w-24 fill-foreground stroke-none"
+            viewBox={`0 0 200 ${windowHeight}`}
+            preserveAspectRatio="none"
+        >
             <motion.path
                 variants={curve}
-                initial='initial'
-                animate='enter'
-                exit='exit'
+                initial="initial"
+                animate="enter"
+                exit="exit"
             />
         </motion.svg>
     );
